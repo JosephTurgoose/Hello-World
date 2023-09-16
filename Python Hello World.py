72028985da512a2e5dd28dbb5521cd4d313eff67
Joseph Turgoose <21jturgoose@gmail.com>
2023-09-15 22:36:33 -0600

    Complete

---
+++ Python Hello World.py
@@ -0,0 +1,10 @@
+
+
+# Let's create a simple statement and send it to the console.
+# To do this, use the "print()" function like I do below:
+
+print("Hello world!")
+
+# Now let's run the script.
+
+# As you can see, "Hello world!" is shown in the console below.
